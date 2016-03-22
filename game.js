e788941667e76d9a0d39e0b52f6e54193907acef
Samuel Bristol <samuel.d.bristol@gmail.com>
2016-03-22 14:19:29 -0500

    Add basic game shell and index page

---
+++ game.js
@@ -0,0 +1,27 @@
+var game = new Phaser.Game(
+  800,
+  600,
+  Phaser.AUTO,
+  'game',
+  { preload: preload, create: create }
+);
+
+function preload() {
+
+    //  You can fill the preloader with as many assets as your game requires
+
+    //  Here we are loading an image. The first parameter is the unique
+    //  string by which we'll identify the image later in our code.
+
+    //  The second parameter is the URL of the image (relative)
+    game.load.image('phaser', 'phaser-logo-small.png');
+
+}
+
+function create() {
+
+    //  This creates a simple sprite that is using our loaded image and
+    //  displays it on-screen
+    game.add.sprite(0, 0, 'phaser');
+
+}
