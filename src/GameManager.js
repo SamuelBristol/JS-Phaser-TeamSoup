e78f7021f52bfc4c43c82b17cf5b7b2262842289
Samuel Bristol <samuel.d.bristol@gmail.com>
2016-04-08 00:49:39 -0500

    Add spawnEnemy function, configures collisionGroups for Player, Enemy and Virus

--- src/GameManager.js
+++ src/GameManager.js
@@ -1,47 +1,105 @@
-var GameManager = function(game, difficulty) {
+var GameManager = function(game) {
   this.game = game;
 
   this.player = null;
   this.enemies = new Array();
+
+  this.collisionGroups = {};
 };
 
 GameManager.prototype.init = function () {
   this._setupWorldPhysics();
-  this._addPlayer();
-  this._addPhysicsObject(this.player, 50, 50);
-  this.player.sprite.body.damping = this.player.damping;
-  this.player.sprite.body.fixedRotation = this.player.fixedRotation;
+
+  // Set up collisionGroups
+  this.collisionGroups["player"] = this.game.physics.p2.createCollisionGroup();
+  this.collisionGroups["virus"] = this.game.physics.p2.createCollisionGroup();
+  this.collisionGroups["enemy"] = this.game.physics.p2.createCollisionGroup();
+
+  this.spawnPlayer();
+
+  // Spawn enemies
+  for(var i = 0; i < 10; i++) {
+    var randomScale = Math.random() * (2 - 0.25 + 1) + 0.25;
+    var enemy = this.spawnEnemy(randomScale);
+    this.enemies.push(enemy);
+  }
+  //
 };
 
 GameManager.prototype.update = function () {
   // Update all objects this GameManager controls
   this.player.update();
+  for(var i = 0; i < this.enemies.length; i++) {
+    this.enemies[i].update();
+  }
 };
 
 // Private functions
-GameManager.prototype._addPlayer = function () {
+GameManager.prototype.spawnPlayer = function () {
   var playerHealth = 2;
   var keyboard = game.input.keyboard.createCursorKeys();
   keyboard.specialKey = game.input.keyboard.addKey(Phaser.KeyCode.Z);
   this.player = new Player(playerHealth, keyboard);
+  this._addPhysicsObject(this.player, 50, 50);
+  this.player.sprite.body.damping = this.player.damping;
+  this.player.sprite.body.fixedRotation = this.player.fixedRotation;
+
+  // Set collision groups
+  this.player.sprite.body.setCollisionGroup(this.collisionGroups["player"]);
+  this.player.sprite.body.collides([
+    this.collisionGroups["virus"],
+    this.collisionGroups["enemy"]
+  ], console.log('player collided'), this);
+  this.player.sprite.body.collideWorldBounds = true;
+  this.game.physics.p2.updateBoundsCollisionGroup();
+
 };
 
-GameManager.prototype.spawnVirus = function () {
-  var x = this.player.sprite.position.x;
-  var y = this.player.sprite.position.y;
-  var scale = 0.5;
+GameManager.prototype.spawnVirus = function (x, y) {
 
   var virus = new Virus();
-  this._addPhysicsObject(virus, x, y - 50);
-  virus.sprite.scale.setTo(scale, scale);
+  this._addPhysicsObject(virus, x, y);
   virus.sprite.tint = virus.tint;
   virus.sprite.alpha = virus.alpha;
+
+  // Set collision groups
+  virus.sprite.body.setCollisionGroup(this.collisionGroups["virus"]);
+  virus.sprite.body.collides([
+    this.collisionGroups["player"],
+    this.collisionGroups["enemy"],
+    this.collisionGroups["virus"]
+  ], console.log('virus collided'), this);
+  virus.sprite.body.collideWorldBounds = true;
+  this.game.physics.p2.updateBoundsCollisionGroup();
 
   return virus;
 };
 
+GameManager.prototype.spawnEnemy = function (scale) {
+  var x = Math.floor(Math.random() * (GAME_WIDTH - 0 + 1)) + 0;
+  var y = Math.floor(Math.random() * (GAME_HEIGHT - 0 + 1)) + 0;
+
+  var enemy = new Enemy(scale);
+  this._addPhysicsObject(enemy, x, y);
+  enemy.sprite.tint = enemy.tint;
+  enemy.sprite.alpha = enemy.alpha;
+
+  // Set collision groups
+  enemy.sprite.body.setCollisionGroup(this.collisionGroups["enemy"]);
+  enemy.sprite.body.collides([
+    this.collisionGroups["player"],
+    this.collisionGroups["virus"],
+    this.collisionGroups["enemy"]
+  ], console.log('enemy collided'), this);
+  enemy.sprite.body.collideWorldBounds = true;
+  this.game.physics.p2.updateBoundsCollisionGroup();
+
+  return enemy;
+};
+
 GameManager.prototype._addPhysicsObject = function(entity, x, y) {
   entity.sprite = game.add.sprite(x, y, entity.name);
+  entity.sprite.scale.setTo(entity.scale, entity.scale);
   this._enablePhysics(entity);
 };
 
@@ -53,5 +111,5 @@ GameManager.prototype._enablePhysics = function(entity) {
 GameManager.prototype._setupWorldPhysics = function () {
   //	Enable p2 physics
   this.game.physics.startSystem(Phaser.Physics.P2JS);
-  this.game.physics.p2.defaultRestitution = 0.5;
+  this.game.physics.p2.defaultRestitution = 0.9;
 };
