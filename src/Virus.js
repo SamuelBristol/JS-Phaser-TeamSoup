1402dfb6273c77f5be59a3e0279157d69039071e
Samuel Bristol <samuel.d.bristol@gmail.com>
2016-04-21 09:29:54 -0500

    Modify Virus.tint, Remove Virus.kill()

--- src/Virus.js
+++ src/Virus.js
@@ -1,16 +1,15 @@
 // Virus class, Extends entity
 function Virus () {
   // Call parent constructor
-  Entity.call(this, 'blob', 1, 'assets/resource/Blob.png', 0.5);
+  Entity.call(this, 'virus', 1, 'assets/resource/Blob.png', 0.5);
 
   // Extra virus members
   this.maxDurationInFrames = 300; // 5 seconds
   this.framesActive = 0;
-  this.isAlive = true;
 
   // Sprite members
   this.alpha = 0.5;
-  this.tint = 0xFFFF00;
+  this.tint = 0x9D7445;
 }
 
 Virus.prototype = Object.create(Entity.prototype);
@@ -19,12 +18,8 @@ Virus.prototype.constructor = Virus;
 Virus.prototype.update = function () {
   this.framesActive++;
 
-  if (this.framesActive > this.maxDurationInFrames) {
+  var maxDurationExceeded = this.framesActive > this.maxDurationInFrames;
+  if (maxDurationExceeded || !this.isAlive()) {
     this.kill();
   }
-};
-
-Virus.prototype.kill = function () {
-  this.isAlive = false;
-  this.sprite.exists = false;
 };
