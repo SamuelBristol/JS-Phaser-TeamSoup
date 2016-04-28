54cd4f884942e37e236a7e0e11bd1e186efba812
Mohr <emohr3@mac-ecb152a-22.cvtc.local>
2016-04-28 11:05:06 -0500

    combined text with states

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -10,13 +10,36 @@ Ameblob.MainMenu.prototype = {
   },
   
   create: function() {
-    game.add.text(300, 300, '- click to start -', {font: "40px Arial", fill: "#fff", align: "center"});
-    console.log('mainmenu');
-    game.input.onDown.add(this.start, this);
+   // Add text here pick positions to add it to
+    var titleText = "AMOEBLOB"
+    var playText = "PLAY";
+    var settingsText = "SETTINGS";
+    var controlsText = "CONTROLS";
+    var creditsText = "CREDITS";
+    
+    // initialize style
+    var style = { font: "bold 72px Arial", fill: "#2f0", boundsAlignH: "center", boundsAlignV: "middle" };
+    
+    // adds title text to screen
+    title = game.add.text(0, -200, titleText, style);
+    
+    // re-assign style with smaller font
+    style = { font: "bold 38px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+    
+    play = game.add.text(0, 0, playText, style);
+    settings = game.add.text(0, 0, settingsText, style);
+    controls = game.add.text(0, 0, controlsText, style);
+    credits = game.add.text(0, 0, creditsText, style);
+    
+    // Setting text bounds
+    title.setTextBounds(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    play.setTextBounds(0, -100, GAME_WIDTH, GAME_HEIGHT);
+    settings.setTextBounds(0, -50, GAME_WIDTH, GAME_HEIGHT);
+    controls.setTextBounds(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    credits.setTextBounds(0, 50, GAME_WIDTH, GAME_HEIGHT);
   },
   
   start: function() {
-    console.log('startgame');
     this.state.start('Ameblob.Game');
   }
 };
