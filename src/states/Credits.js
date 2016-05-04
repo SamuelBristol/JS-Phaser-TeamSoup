8692be6dd832b67dbc8b0c82f243348f9360d495
Romeo Salinas Jr <Romeo Salinas Jr>
2016-05-04 15:53:41 -0500

    add credits for sound/music under CC BY 3.0

--- src/states/Credits.js
+++ src/states/Credits.js
@@ -6,9 +6,11 @@ Ameblob.Credits.prototype = {
         
         // Add text here pick positions to add it to
         var titleText = "CREDITS"
-        var samText = "Samuel D. Bristol: Programmer";
-        var romeoText = "Romeo Salinas Jr: Lead";
-        var ericText = "Eric Mohr: Menus";
+        var samText = "Samuel D. Bristol: Main Developer";
+        var romeoText = "Romeo Salinas Jr: Co-Developer";
+        var ericText = "Eric J. Mohr: Co-Developer";
+        var jayManText = "Jay Man: Music under CC BY 3.0";
+        var mikeKoenigText = "Mike Koenig: Sound under CC BY 3.0";
         var backText = "BACK";
         
         var style = { font: "bold 72px Arial", fill: "#2f0", boundsAlignH: "center", boundsAlignV: "middle" };
@@ -20,13 +22,16 @@ Ameblob.Credits.prototype = {
         samLabel = game.add.text(0, 0, samText, style);
         romeoLabel = game.add.text(0, 0, romeoText, style);
         ericLabel = game.add.text(0, 0, ericText, style);
+        jayLabel = game.add.text(0, 0, jayManText, style);
+        mikeLabel = game.add.text(0, 0, mikeKoenigText, style);
         back = game.add.text(0, 0, backText, style);
         
         title.setTextBounds(0, 0, GAME_WIDTH, GAME_HEIGHT);
         samLabel.setTextBounds(0, -100, GAME_WIDTH, GAME_HEIGHT);
         romeoLabel.setTextBounds(0, -50, GAME_WIDTH, GAME_HEIGHT);
         ericLabel.setTextBounds(0, 0, GAME_WIDTH, GAME_HEIGHT);
-        
+        jayLabel.setTextBounds(0, 50, GAME_WIDTH, GAME_HEIGHT);
+        mikeLabel.setTextBounds(0, 100, GAME_WIDTH, GAME_HEIGHT);
         back.setTextBounds(0, 200, GAME_WIDTH, GAME_HEIGHT);
         
         goBackHover();
